0a9f6bc1d1ec7656af903b371bdafa44e407b3f7
Shalom Ram <shalom.ram1412@gmail.com>
2025-01-21 16:29:18 +0530

    Initial commit

---
+++ first/first/views.py
@@ -0,0 +1,75 @@
+from django.http import HttpResponse 
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.views.decorators.csrf import csrf_protect
+import time
+from django.db import connection
+
+curl=settings.CURRENT_URL
+
+def home(request):
+    return render(request,"home.html",{'curl':curl})
+
+def about(request):
+    return render(request,"about.html",{'curl':curl})
+
+def contact(request):
+    return render(request,"contact.html",{'curl':curl})
+
+def service(request):
+    return render(request,"service.html",{'curl':curl})
+
+def register(request):
+    if request.method=='GET':
+        return render(request,"register.html",{'curl':curl,'msg':''})
+    else:
+        name=request.POST.get("name")
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        mobile=request.POST.get("mobile")
+        address=request.POST.get("address")
+        city=request.POST.get("city")
+        gender=request.POST.get("gender")
+        print(name)
+        print(gender)
+        info=time.asctime(time.localtime(time.time()))
+
+        #Create a query to insert record in database
+        sql="insert into register values(NULL,'%s','%s','%s','%s','%s','%s','%s','user',1,'%s')"%(name,username,password,mobile,address,city,gender,info)
+
+        #Execute a query using cursor instance
+        cursor=connection.cursor()
+        cursor.execute(sql)
+
+        return render(request,"register.html",{'curl':curl,'msg':'Form Submitted'})
+
+def login(request):
+    if request.method=='GET':
+        return render(request,"login.html",{'curl':curl,'msg':''})
+    else:
+        username=request.POST.get("username")
+        password=request.POST.get("Password")
+
+        #Query to fetch user detail from database
+        sql="select * from register where username='%s' and password='%s' and status=1"%(username,password)
+        cursor=connection.cursor()
+        cursor.execute(sql)
+
+        #Fetch record from cursor
+        userDetails=cursor.fetchone()
+        print(userDetails)
+        
+        if userDetails!=None:
+            if userDetails[8]=="user":
+                return redirect(curl+'user/')
+            else:
+                return redirect(curl+'myadmin/')
+         
+        else:
+            return render(request,"login.html",{'curl':curl,'msg':'invalid user please login again'})
+        
+def adminhome(request):
+    return render(request,"adminhome.html",{'curl':curl})
+
+def userhome(request):
+    return render(request,"userhome.html",{'curl':curl})
